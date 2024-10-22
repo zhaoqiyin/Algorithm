c86d0853ae3a81fc6f28477a951cefa35f749255
zhaoqi.yin <zhaoqi.yin@colorv.com>
2024-10-22 11:55:34 +0800

    暴力递归

--- src/class08/Code03_PrintAllPermutations.java
+++ src/class08/Code03_PrintAllPermutations.java
@@ -14,17 +14,30 @@ public class Code03_PrintAllPermutations {
         }
         char[] chs = str.toCharArray();
         process(chs, 0, res);
-        res.sort(null);
         return res;
     }
 
-    public static void process(char[] chs, int i, ArrayList<String> res) {
-        if (i == chs.length) {
-            res.add(String.valueOf(chs));
+    // str[i...]范围上，所有的字符，都可以在i位置上，后续都去尝试
+    // str[0..i-1]范围上，是之前做的选择
+    // 请把所有的字符串形成的全排列，加入到res里去
+    public static void process(char[] str, int i, ArrayList<String> res) {
+        if (i == str.length) {
+            res.add(String.valueOf(str));
         }
-        boolean[] visit = new boolean[26];
-        for (int j = i; j < chs.length; j++) {
+        boolean[] visit = new boolean[26]; // visit[0]表示'a'有没有试过
+        for (int j = i; j < str.length; j++) {
+            if (!visit[str[j] - 'a']) {
+                visit[str[j] - 'a'] = true;
+                swap(str, i, j);
+                process(str, i + 1, res);
+                swap(str, i, j);
+            }
+        }
+    }
 
-        }
+    public static void swap(char[] chs, int i, int j) {
+        char temp = chs[i];
+        chs[i] = chs[j];
+        chs[j] = temp;
     }
 }
---
+++ src/class08/Code06_ConvertToLetterString.java
@@ -0,0 +1,48 @@
+package class08;
+
+/**
+ * 规定1和A对应、2和B对应、3和C对应...
+ * 吧呢图个数字字符串比如“111”，就可以转化成“AAA”、“KA”、“AK”。
+ * 给定一个只有数字字符组成的字符串str，返回有多少种转化结果
+ */
+public class Code06_ConvertToLetterString {
+
+    public static int number(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        return process(str.toCharArray(), 0);
+    }
+
+    // i之前的位置，如何转化已经做过决定了
+    // i...有多少种转化的结果
+    public static int process(char[] str, int i) {
+        if (i == str.length) {
+            return 1;
+        }
+        if (str[i] == '0') {
+            return 0;
+        }
+        if (str[i] == '1') {
+            int res = process(str, i + 1); // i自己作为单独的部分，后续有多少种方法
+            if (i + 1 < str.length) {
+                res += process(str, i + 2); // (i和i+1)作为单独的部分，后续有多少种方法
+            }
+            return res;
+        }
+        if (str[i] == '2') {
+            int res = process(str, i + 1); // i自己作为单独的部分，后续有多少种方法
+            // (i和i+1)作为单独的部分并且没有超过26，后续有多少种方法
+            if (i + 1 < str.length && str[i + 1] >= '0' && str[i + 1] <= '6') {
+                res += process(str, i + 2);
+            }
+            return res;
+        }
+        // str[i] == '3' ~ '9'
+        return process(str, i + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(number("11111"));
+    }
+}
